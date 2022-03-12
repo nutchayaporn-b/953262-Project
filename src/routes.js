6e0736b7dd51d15af67fba208d40fcc454d249e1
mai_ammi <mai_ammi@hotmail.com>
2022-03-13 03:16:40 +0700

    Setup workflow

--- src/routes.js
+++ src/routes.js
@@ -10,32 +10,40 @@ import Products from './pages/Products';
 import Blog from './pages/Blog';
 import User from './pages/User';
 import NotFound from './pages/Page404';
+import { useAuth } from './context/AuthContext';
 
 // ----------------------------------------------------------------------
 
 export default function Router() {
-  return useRoutes([
-    {
-      path: '/dashboard',
-      element: <DashboardLayout />,
-      children: [
-        { path: 'app', element: <DashboardApp /> },
-        { path: 'user', element: <User /> },
-        { path: 'products', element: <Products /> },
-        { path: 'blog', element: <Blog /> }
-      ]
-    },
-    {
-      path: '/',
-      element: <LogoOnlyLayout />,
-      children: [
-        { path: '/', element: <Navigate to="/dashboard/app" /> },
-        { path: 'login', element: <Login /> },
-        { path: 'register', element: <Register /> },
-        { path: '404', element: <NotFound /> },
-        { path: '*', element: <Navigate to="/404" /> }
-      ]
-    },
-    { path: '*', element: <Navigate to="/404" replace /> }
-  ]);
+    return useRoutes([
+        {
+            path: '/',
+            element: <DashboardLayout />,
+            children: [
+                { path: 'dashboard', element: <DashboardApp /> },
+                { path: 'customer', element: <Products /> },
+                { path: 'kitchen', element: <Blog /> },
+                { path: 'login', element: <Login /> },
+                { path: 'register', element: <Register /> },
+            ],
+        },
+        {
+            path: '/customer',
+            element: <DashboardLayout />,
+            children: [
+                { path: 'order', element: <Products /> },
+                { path: 'checkout', element: <Blog /> },
+            ],
+        },
+        {
+            path: '/',
+            element: <LogoOnlyLayout />,
+            children: [
+                { path: '/', element: <Navigate to="/dashboard" /> },
+                { path: '404', element: <NotFound /> },
+                { path: '*', element: <Navigate to="/404" /> },
+            ],
+        },
+        { path: '*', element: <Navigate to="/404" replace /> },
+    ]);
 }
